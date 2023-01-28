795d1f4467e86b695221b2cdb8610aa48a53f2a8
aLVINlEE9 <fldh3746@gmail.com>
2023-01-28 20:20:14 +0900

    fix: cmake

--- stl/main.cpp
+++ stl/main.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <iostream>
+
 struct Add{
 	Add(int toAdd) : _toAdd(toAdd) {}
 	int operator()(int x){
@@ -14,4 +17,5 @@ int main(){
 	int result_13 = add_13(2);
 	std::vector<int> v(10, 10);
 	std::transform(v.begin(), v.end(), v.begin(), add_42);
+	std::cout << result_13 << " " << result_42 << std::endl;
 }
---
+++ test/iterator_test.cpp
@@ -0,0 +1,9 @@
+
+#include "container.h"
+#include "gtest/gtest.h"
+
+namespace {
+	TEST(config_test, allow_method) {
+		EXPECT_EQ(false, false);
+	}
+}
