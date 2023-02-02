e01f17fb37208987ccd7a95befff583e1aacdfb6
aLVINlEE9 <fldh3746@gmail.com>
2023-02-02 16:57:34 +0900

    Feat: iterator_tags 추가
    input_iterator_tag
    forward_iterator_tag
    bidirectional_iterator_tag
    random_access_iterator_tag
    output_iterator_tag
    빈 클래스 추가

--- stl/iterator/iterator_trait.hpp
+++ stl/iterator/iterator_trait.hpp
@@ -7,9 +7,14 @@
 
 #include <cstddef>
 
-// Todo: random_access_iterator_tag 만들기
-
 namespace ft {
+
+// Empty classes to identify the category of an iterators
+struct input_iterator_tag {};
+struct forward_iterator_tag {};
+struct bidirectional_iterator_tag {};
+struct random_access_iterator_tag {};
+struct output_iterator_tag {};
 
 // generic definition
 template<class Iterator>
