43bfd2548f6798c0d1aa9a138452124dbfaf4491
aLVINlEE9 <fldh3746@gmail.com>
2023-02-03 15:43:39 +0900

    Feat: declared uninitialized templates
    uninitialized_fill_n
    uninitialized_fill
    uninitialized_copy_n
    uninitialized_copy

---
+++ stl/vector/uninitialized.hpp
@@ -0,0 +1,30 @@
+//
+// Created by Alvin Lee on 2023/02/03.
+//
+
+#ifndef FT_CONTAINER_STL_VECTOR_UNINITIALIZED_HPP_
+#define FT_CONTAINER_STL_VECTOR_UNINITIALIZED_HPP_
+
+namespace ft {
+
+// TODO uninitialized implement
+//  - uninitialized_fill_n()
+//  - uninitialized_fill()
+//  - uninitialized_copy_n()
+//  - uninitialized_copy()
+
+template<class ForwardIterator, class Size, class T>
+void uninitialized_fill_n(ForwardIterator first, Size n, const T &x);
+
+template<class ForwardIterator, class T>
+void uninitialized_fill(ForwardIterator first, ForwardIterator last, const T &x);
+
+template<class InputIterator, class Size, class ForwardIterator>
+ForwardIterator uninitialized_copy_n(InputIterator first, Size n, ForwardIterator result);
+
+template<class InputIterator, class ForwardIterator>
+ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result);
+
+}
+
+#endif //FT_CONTAINER_STL_VECTOR_UNINITIALIZED_HPP_
