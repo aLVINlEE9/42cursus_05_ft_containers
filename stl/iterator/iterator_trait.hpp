032585ece2a615be330e57ccddb7a16d94463d40
aLVINlEE9 <fldh3746@gmail.com>
2023-02-03 15:49:12 +0900

    Fix: iterator_tag 상속되게 하였음

--- stl/iterator/iterator_trait.hpp
+++ stl/iterator/iterator_trait.hpp
@@ -11,10 +11,10 @@ namespace ft {
 
 // Empty classes to identify the category of an iterators
 struct input_iterator_tag {};
-struct forward_iterator_tag {};
-struct bidirectional_iterator_tag {};
-struct random_access_iterator_tag {};
 struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
 // generic definition
 template<class Iterator>
