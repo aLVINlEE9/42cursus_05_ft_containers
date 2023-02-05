84470e96240f95f886feee620ffbb042b9d1bc0d
aLVINlEE9 <fldh3746@gmail.com>
2023-02-05 21:57:27 +0900

    Feat: is_integral 구현

---
+++ stl/utils/is_integral.hpp
@@ -0,0 +1,56 @@
+//
+// Created by Alvin Lee on 2023/02/05.
+//
+
+#ifndef FT_CONTAINER_STL_UTILS_IS_INTEGRAL_HPP_
+#define FT_CONTAINER_STL_UTILS_IS_INTEGRAL_HPP_
+
+#include <integral_constant.hpp>
+
+namespace ft {
+
+template<typename T>
+struct is_integral : public integral_constant<bool, false> {};
+
+template<>
+struct is_integral<bool> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<char> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<signed char> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<unsigned char> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<wchar_t> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<short> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<unsigned short> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<int> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<unsigned int> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<long> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<unsigned long> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<long long> : public integral_constant<bool, true> {};
+
+template<>
+struct is_integral<unsigned long long> : public integral_constant<bool, true> {};
+
+}
+
+#endif //FT_CONTAINER_STL_UTILS_IS_INTEGRAL_HPP_
