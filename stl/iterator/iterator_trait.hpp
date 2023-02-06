e5aad838afc1906399b4a33e8fbe5760b2d2617e
aLVINlEE9 <fldh3746@gmail.com>
2023-02-06 18:00:47 +0900

    Feat: iterator_trait ifdef 추가
    compile 때 FT define 하면, ft 내부 iterator_tag 사용
    그냥 하면, std::iterator_tag 사용

--- stl/iterator/iterator_trait.hpp
+++ stl/iterator/iterator_trait.hpp
@@ -8,13 +8,20 @@
 #include <cstddef>
 
 namespace ft {
-
 /// Empty classes to identify the category of an iterators
+#ifdef FT
 struct input_iterator_tag {};
 struct output_iterator_tag {};
 struct forward_iterator_tag : public input_iterator_tag {};
 struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+#else
+typedef std::input_iterator_tag input_iterator_tag;
+typedef std::output_iterator_tag output_iterator_tag;
+typedef std::forward_iterator_tag forward_iterator_tag;
+typedef std::bidirectional_iterator_tag bidirectional_iterator_tag;
+typedef std::random_access_iterator_tag random_access_iterator_tag;
+#endif
 
 /// generic definition
 template<class Iterator>
